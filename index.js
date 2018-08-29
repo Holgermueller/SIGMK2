0e78b1500423bd4a474b35eb90a7b03bf6d1c555
Holger Mueller <9jholger@gmail.com>
2018-08-28 21:28:29 -0500

    got time to auto update

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
-const date = new Date();
-console.log(date);
+function updatingTime() {
+    const currentTime = new Date();
+    let hours = currentTime.getHours();
+    let minutes = currentTime.getMinutes();
+    let seconds = currentTime.getSeconds();
 
-document.getElementById('time').innerHTML = date;
+    if ( seconds < 10) {
+        seconds = '0' + seconds;
+    }
+
+    if (minutes < 10 ) {
+        minutes = '0' + minutes;
+    }
+
+    let updatingHours = hours + ': ' + minutes + ': ' + seconds + ' ';
+    if (hours > 11) {
+        updatingHours += 'PM';
+    } else {
+        updatingHours += 'AM'
+    }
+    document.getElementById('time').innerHTML = updatingHours;
+}
+setInterval(updatingTime, 1000);
