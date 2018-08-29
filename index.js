9642429b2865e471146700dba224ab053b5e39ba
Holger Mueller <9jholger@gmail.com>
2018-08-28 21:54:49 -0500

    began location function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function updatingTime() {
     const currentTime = new Date();
-    let hours = currentTime.getHours();
+    let hours = currentTime.getHours() % 12;
     let minutes = currentTime.getMinutes();
     let seconds = currentTime.getSeconds();
 
@@ -19,5 +19,21 @@ function updatingTime() {
         updatingHours += 'AM'
     }
     document.getElementById('time').innerHTML = updatingHours;
-}
+};
 setInterval(updatingTime, 1000);
+
+const myLocation = document.getElementById('location');
+
+function userLocation(){
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        myLocation.innerHTML = 'Geolocation is not supported by this browser.';
+    }
+};
+
+function showPosition(position) {
+    
+}
+
+console.log(navigator.geolocation.getCurrentPosition(showPosition));
