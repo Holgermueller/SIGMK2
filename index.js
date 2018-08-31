60f5725e36afbaf8a9ea9550358adfe743631229
Holger Mueller <9jholger@gmail.com>
2018-08-30 20:58:23 -0500

    added form

--- index.js
+++ index.js
@@ -1,32 +1,45 @@
+// Get current time:
 function updatingTime() {
-    const currentTime = new Date();
-    let hours = currentTime.getHours() % 12;
-    let minutes = currentTime.getMinutes();
-    let seconds = currentTime.getSeconds();
+	const currentTime = new Date();
+	let hours = currentTime.getHours() % 12;
+	let minutes = currentTime.getMinutes();
+	let seconds = currentTime.getSeconds();
 
-    seconds = seconds < 10 ? '0' + seconds : seconds;
+	seconds = seconds < 10 ? '0' + seconds : seconds;
 
-    minutes = minutes < 10 ? '0' + minutes : minutes;
+	minutes = minutes < 10 ? '0' + minutes : minutes;
 
-    let updatingHours = hours + ': ' + minutes + ': ' + seconds + ' ';
-    hours >= 12 ? updatingHours += 'PM' : updatingHours += 'AM'
+	let updatingHours = hours + ':' + minutes + ':' + seconds + ' ';
 
-    document.getElementById('time').innerHTML = updatingHours;
+	document.getElementById('time').innerHTML = updatingHours;
 };
 setInterval(updatingTime, 1000);
+
+// Get location:
 
 const myLocation = document.getElementById('location');
 
 function userLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-        myLocation.innerHTML = 'Geolocation is not supported by this browser.';
-    }
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(showPosition);
+	} else {
+		myLocation.innerHTML = 'Geolocation is not supported by this browser.';
+	}
 };
+console.log(navigator.geolocation);
+
 
 function showPosition(position) {
 
 }
 
-console.log(navigator.geolocation.getCurrentPosition(showPosition));
+// get weather forcasts
+const getLocationFromForm = document.querySelector('.query-location');
+
+function getLocation(e) {
+	e.preventDefault();
+	document.getElementById('forcastLocation').value;
+	console.log(e.value);
+}
+
+getLocationFromForm.addEventListener('submit', getLocation);
