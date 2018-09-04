0e80b2b21032192405a21997e5fb702442cd7c07
Holger Mueller <9jholger@gmail.com>
2018-09-04 17:06:46 -0500

    got full date and time on dom

--- index.js
+++ index.js
@@ -6,11 +6,41 @@ function updatingTime() {
 	let seconds = currentTime.getSeconds();
 
 	seconds = seconds < 10 ? '0' + seconds : seconds;
-
 	minutes = minutes < 10 ? '0' + minutes : minutes;
 
+	let daysOfWeek = ['Sunday',
+		'Monday',
+		'Tuesday',
+		'Wednesday',
+		'Thursday',
+		'Friday',
+		'Saturday'];
+
+	let day = daysOfWeek[currentTime.getDay()];
+
+	let months = [
+		'January',
+		'February',
+		'March',
+		'April',
+		'May',
+		'June',
+		'July',
+		'August',
+		'September',
+		'October',
+		'November',
+		'December'
+	];
+
+	let month = months[currentTime.getMonth()];
+	let currentDay = currentTime.getDate();
+	let year = currentTime.getFullYear();
+
+	let date = day + ' ' + month + ' ' + currentDay + ', ' + year;
 	let updatingHours = hours + ':' + minutes + ':' + seconds + ' ';
 
+	document.getElementById('date').innerHTML = date;
 	document.getElementById('time').innerHTML = updatingHours;
 };
 setInterval(updatingTime, 1000);
