4fff0206d2271cdcbebad193593d06a2d6247ee4
Holger Mueller <9jholger@gmail.com>
2018-09-04 15:39:26 -0500

    got temps on DOM

--- weather.js
+++ weather.js
@@ -1,7 +1,9 @@
 $(document).ready(() => {
 
+	const APIKey = '94d5b3ebbc302231ae85460cfe0af984';
+
 	navigator.geolocation ?
-		navigator.geolocation.getCurrentPosition(showPosition):
+		navigator.geolocation.getCurrentPosition(showPosition) :
 		console.log('not supported');
 
 	function showPosition(position) {
@@ -9,17 +11,24 @@ $(document).ready(() => {
 		const Long = position.coords.longitude;
 
 
-		let url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + Lat + '&lon=' + Long + '*********';
+		let url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + Lat + '&lon=' + Long + '&APPID=' + APIKey;
 
 		$.ajax({
-			url:url,
+			url: url,
 			method: 'GET',
 			success: data => {
 				console.log(data);
+				// convert temps from kelvin:
+					let K = parseFloat(data.main.temp);
+					let kToF = (((K - 273.15) * 1.8) + 32);
+					let F = Math.round(kToF);
+					let kToC = K - 273.15;
+					let C = Math.round(kToC)
+					$('#currentWeather').append(`Temp: ${F}  F / ${C}  C`)
+						.append(`Humidity: ${data.main.humidity} %`);
 			}
 		})
 	}
-
 });
 
 // get weather for that specific time an place
