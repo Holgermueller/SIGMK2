ed3a5f9b4493047740e3313f3103e101ea377ef6
Holger Mueller <9jholger@gmail.com>
2018-09-05 22:08:23 -0500

    got icon on DOM

--- weather.js
+++ weather.js
@@ -19,22 +19,35 @@ $(document).ready(() => {
 			success: data => {
 				console.log(data);
 				$('#currentLocation').append(`<h3>${data.name}</h3>`);
+
+				// get icon on DOM
+				let displayIcon = data.weather[0].icon;
+				let weatherIconURL = 'http://openweathermap.org/img/w/' + displayIcon + '.png';
+				$('#icon').attr('src', weatherIconURL);
+
 				// convert temps from kelvin:
 				let K = parseFloat(data.main.temp);
 				let kToF = (((K - 273.15) * 1.8) + 32);
 				let F = Math.round(kToF);
 				let kToC = K - 273.15;
-				let C = Math.round(kToC)
-				$('#currentWeather').append(`<div>Temp: ${F}  F / ${C}  C</div>`)
-					.append(`<div>${data.weather[0].main}</div>`)
-					.append(`<h5>${data.weather[0].description}</h5>`)
-					.append(`<div>Humidity: ${data.main.humidity} %</div>`);
+				let C = Math.round(kToC);
+
+				// convert sunrise to actual time
+				let sunriseStamp = parseFloat(data.sys.sunrise);
+				let stampDate = new Date(sunriseStamp * 1000);
+				let sunriseTime = stampDate.toLocaleTimeString();
+
+				let sunsetStamp = parseFloat(data.sys.sunset);
+				let dateStamp = new Date(sunsetStamp * 1000);
+				let sunsetTime = dateStamp.toLocaleTimeString();
+
+				$('#currentWeather').append(`<div>${data.weather[0].main}</div>`)
+					.append(`<div>Temp: ${F}&#176  F / ${C}&#176  C</div>`)
+					.append(`<h5>Description ${data.weather[0].description}</h5>`)
+					.append(`<div>Humidity: ${data.main.humidity} %</div>`)
+					.append(`<div>Sunrise: ${sunriseTime} | Sunset: ${sunsetTime}</div>`)
 
 			}
 		});
 	}
 });
-
-// get weather for that specific time an place
-
-// display weather
