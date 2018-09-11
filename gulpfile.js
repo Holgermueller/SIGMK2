20fd1a960e121ef3757cbd94bf44b8e5235eb88e
Holger Mueller <9jholger@gmail.com>
2018-09-10 22:24:02 -0500

    got gulp less working

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,11 @@
 const gulp = require('gulp');
 
 const less = require('gulp-less');
+const path = require('path');
+
+// compile less files
+gulp.task('less', () => {
+    return gulp.src('./assets/**/*.less')
+    .pipe(less())
+    .pipe(gulp.dest('./assets/css'));
+});
