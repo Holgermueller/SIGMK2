e8f68a66ad94f7c5c8cb2cec27b4e8119f11ea58
Holger Mueller <9jholger@gmail.com>
2019-01-12 14:53:11 -0600

    switched time fucntionality to moment

--- js/time/time.js
+++ js/time/time.js
@@ -1,46 +1,6 @@
-// Get current time:
-function updatingTime() {
-	const currentTime = new Date();
-	let hours = currentTime.getHours() % 12;
-	let minutes = currentTime.getMinutes();
-	let seconds = currentTime.getSeconds();
-
-	seconds = seconds < 10 ? '0' + seconds : seconds;
-	minutes = minutes < 10 ? '0' + minutes : minutes;
-
-	let daysOfWeek = ['Sunday',
-		'Monday',
-		'Tuesday',
-		'Wednesday',
-		'Thursday',
-		'Friday',
-		'Saturday'];
-
-	let day = daysOfWeek[currentTime.getDay()];
-
-	let months = [
-		'January',
-		'February',
-		'March',
-		'April',
-		'May',
-		'June',
-		'July',
-		'August',
-		'September',
-		'October',
-		'November',
-		'December'
-	];
-
-	let month = months[currentTime.getMonth()];
-	let currentDay = currentTime.getDate();
-	let year = currentTime.getFullYear();
-
-	let date = day + ', ' + month + ' ' + currentDay + ', ' + year;
-	let updatingHours = hours + ':' + minutes + ':' + seconds + ' ';
-
-	document.getElementById('date').innerHTML = date;
-	document.getElementById('time').innerHTML = updatingHours;
-};
-setInterval(updatingTime, 1000);
+function updateTime() {
+  moment();
+  const CURRENTDATEANDTIME = moment().format("ddd, MMM Do, YYYY <br> hh:mm:ss A");
+  $(".date-time").html(CURRENTDATEANDTIME);
+}
+setInterval(updateTime, 1000);
