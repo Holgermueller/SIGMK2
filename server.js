862fb23ba543e3a56f50299ddfb896a08975a28c
Holger Mueller <9jholger@gmail.com>
2019-01-14 20:23:09 -0600

    got page on dom

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const EXPRESS = require('express');
+const EXPHBS = require('express-handlebars');
 
 const APP = EXPRESS();
 const PORT = process.env.PORT || 8080;
 
+APP.use(EXPRESS.static(__dirname + 'public'));
+
+APP.engine('handlebars', EXPHBS({ defaultLayout: 'main'}));
+APP.set('view engine', 'handlebars');
+
+require('./routes/routes.js')(APP);
+
 APP.listen(PORT, () => {
   console.log('server listening on ', PORT);
-})
+});
