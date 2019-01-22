499ab5654339979b987c3a8f619a291181e1ee29
Holger Mueller <9jholger@gmail.com>
2019-01-22 17:00:35 -0600

    major header styling complete

--- public/js/time/time.js
+++ public/js/time/time.js
@@ -1,6 +1,6 @@
 function updateTime() {
   moment();
   const CURRENTDATEANDTIME = moment().format("ddd, MMM Do, YYYY <br> hh:mm:ss A");
-  $(".date-time").html(CURRENTDATEANDTIME);
+  $("#currentTime").html(CURRENTDATEANDTIME);
 }
 setInterval(updateTime, 1000);
