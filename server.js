c7a699fe6176e12f99b3c9d3e29042451f32549f
Holger Mueller <9jholger@gmail.com>
2019-01-24 12:32:33 -0600

    added express minify

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const EXPRESS = require('express');
 const EXPHBS = require('express-handlebars');
 const lessMiddleware = require('less-middleware');
+const minify = require('express-minify');
 const reload = require('reload');
 
 const APP = EXPRESS();
 const PORT = process.env.PORT || 8080;
 
 APP.use(lessMiddleware('public'));
+APP.use(minify());
 APP.use(EXPRESS.static('public'));
 
 APP.engine('handlebars', EXPHBS({ defaultLayout: 'main'}));
