3984815f41ffd023015fbc9e85693a69c0238abe
Holger Mueller <9jholger@gmail.com>
2019-02-01 22:01:42 -0600

    added morgan

--- server.js
+++ server.js
@@ -2,16 +2,31 @@ const EXPRESS = require('express');
 const EXPHBS = require('express-handlebars');
 const lessMiddleware = require('less-middleware');
 const minify = require('express-minify');
-const uglify = require('uglify-es');
+const uglifyEs = require('uglify-es');
 const reload = require('reload');
 const compression = require('compression');
+const morgan = require('morgan');
 
 const APP = EXPRESS();
 const PORT = process.env.PORT || 8080;
 
+APP.use(morgan('tiny'));
+
+const myErrorHanlder = (errorInfo, callback) => {
+  console.log(errorInfo);
+  if (errorInfo.stage === 'minify'){
+    callback(errorInfo.error, JSON.stringify(errorInfo.error));
+    return;
+  }
+  callback(errorInfo.error, errorInfo.body);
+};
+
 APP.use(compression());
 APP.use(lessMiddleware('public'));
-APP.use(minify());
+APP.use(minify({
+  uglifyJS: uglifyEs,
+  errorHandler: myErrorHanlder
+}));
 APP.use(EXPRESS.static('public'));
 
 APP.engine('handlebars', EXPHBS({ defaultLayout: 'main'}));
