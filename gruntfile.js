3dfb90a2abd320c5250edf804d8cd003b698711a
Holger Mueller <9jholger@gmail.com>
2019-09-17 15:12:44 -0500

    updated node packages

--- gruntfile.js
+++ gruntfile.js
@@ -1,19 +1,17 @@
-'use strict';
+"use strict";
 module.exports = grunt => {
-
   grunt.initConfig({
-
     watch: {
       less: {
-        files: ['public/assets/less/*.less'],
-        tasks: ['less', 'cssmin'],
+        files: ["public/assets/less/*.less"],
+        tasks: ["less", "cssmin"],
         options: {
           livereload: true
         }
       },
       scripts: {
-        files: ['public/js/**/*.js'],
-        tasks: ['browserify','uglify'],
+        files: ["public/js/**/*.js"],
+        tasks: ["browserify", "uglify"],
         options: {
           livereload: true
         }
@@ -23,40 +21,40 @@ module.exports = grunt => {
     less: {
       development: {
         options: {
-          paths: ['public/assets/less']
+          paths: ["public/assets/less"]
         },
         files: {
-          'public/assets/css/style.css': 'public/assets/less/style.less'
+          "public/assets/css/style.css": "public/assets/less/style.less"
         }
       }
     },
 
     cssmin: {
       my_target: {
-        files: [{
-          expand: true,
-          cwd: 'public/assets/css',
-          src: ["*.css", '!*.min.css'],
-          dest: 'public/assets/css',
-          ext: '.min.css'
-        }]
+        files: [
+          {
+            expand: true,
+            cwd: "public/assets/css",
+            src: ["*.css", "!*.min.css"],
+            dest: "public/assets/css",
+            ext: ".min.css"
+          }
+        ]
       }
     },
 
     uglify: {
       my_target: {
         files: {
-          'public/js/build/ugly.min.js': ['public/js/build/build.js']
+          "public/js/build/ugly.min.js": ["public/js/build/build.js"]
         }
       }
     },
 
     browserify: {
       build: {
-        src: [
-          'public/js/**/*.js'
-        ],
-        dest: 'public/js/build/build.js',
+        src: ["public/js/**/*.js"],
+        dest: "public/js/build/build.js",
         watch: true,
         keepAlive: true
       }
@@ -64,29 +62,25 @@ module.exports = grunt => {
 
     browserSync: {
       bsFiles: {
-        src: [
-          'public/assets/css/*.min.css',
-          'public/js/**/*.min.js'
-        ]
+        src: ["public/assets/css/*.min.css", "public/js/**/*.min.js", "views"]
       },
       options: {
         livereload: true,
         watchTask: true,
-        watchEvents: ['add', 'change', 'unlink', 'addDir', 'unlinkDir'],
+        watchEvents: ["add", "change", "unlink", "addDir", "unlinkDir"],
         server: {
-          baseDir: './public'
+          baseDir: "./views"
         }
       }
     }
-
   });
 
-  grunt.loadNpmTasks('grunt-uncss');
-  grunt.loadNpmTasks('grunt-browser-sync');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-uglify-es');
-  grunt.loadNpmTasks('grunt-browserify');
-  grunt.task.registerTask('default', ['browserSync', 'watch']);
+  grunt.loadNpmTasks("grunt-uncss");
+  grunt.loadNpmTasks("grunt-browser-sync");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-contrib-less");
+  grunt.loadNpmTasks("grunt-contrib-cssmin");
+  grunt.loadNpmTasks("grunt-contrib-uglify-es");
+  grunt.loadNpmTasks("grunt-browserify");
+  grunt.task.registerTask("default", ["browserSync", "watch"]);
 };
--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-const EXPRESS = require('express');
-const EXPHBS = require('express-handlebars');
-const compression = require('compression');
-const morgan = require('morgan');
+const EXPRESS = require("express");
+const EXPHBS = require("express-handlebars");
+const compression = require("compression");
+const morgan = require("morgan");
 
 const APP = EXPRESS();
 const PORT = process.env.PORT || 8080;
 
-APP.use(morgan('tiny'));
+APP.use(morgan("tiny"));
 
 APP.use(compression());
-APP.use(EXPRESS.static('public'));
+APP.use(EXPRESS.static("public"));
 
-APP.engine('handlebars', EXPHBS({ defaultLayout: 'main'}));
-APP.set('view engine', 'handlebars');
+APP.engine("handlebars", EXPHBS({ defaultLayout: "main" }));
+APP.set("view engine", "handlebars");
 
-require('./routes/routes.js')(APP);
+require("./routes/routes.js")(APP);
 
 APP.listen(PORT, () => {
-  console.log('server listening on ', PORT);
+  console.log("server listening on ", PORT);
 });
