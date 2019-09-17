2606540230b3e280406fbea4afe965f51f30d90b
Holger Mueller <9jholger@gmail.com>
2019-09-17 16:47:10 -0500

    removed unnecessary code

--- routes/routes.js
+++ routes/routes.js
@@ -1,16 +1,9 @@
 module.exports = app => {
-
-  app.get('/', (req, res) => {
-    res.render('index');
+  app.get("/", (req, res) => {
+    res.render("index");
   });
 
-  app.get('/fiveDayForecast', (req, res) => {
-    res.render('fiveDayForecast');
+  app.get("/fiveDayForecast", (req, res) => {
+    res.render("fiveDayForecast");
   });
-
-  app.get('/map', (req, res) => {
-    res.render('map');
-  });
-
-  // No code below this line
 };
