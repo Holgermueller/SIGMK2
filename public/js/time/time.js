f236a77fd9534cbf936cfcffaffe2341f90e1f0e
holgermueller <9jholger@gmail.com>
2022-09-20 17:25:10 -0500

    starting over with a new project

--- public/js/time/time.js
+++
@@ -1,6 +0,0 @@
-function updateTime() {
-  moment();
-  const CURRENTDATEANDTIME = moment().format("ddd, MMM Do, YYYY <br> hh:mm:ss A");
-  $("#currentTime").html(CURRENTDATEANDTIME);
-}
-setInterval(updateTime, 1000);
