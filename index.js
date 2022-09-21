3c4b0e2d8c16f759a35999670ca13a86bd413e81
holgermueller <9jholger@gmail.com>
2022-09-20 20:17:56 -0500

    can grab random story elements

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const cube = document.querySelector(".cube");
+let currentClass = "";
+
 const protagonists = [
   "Magician",
   "Professor",
@@ -33,3 +36,43 @@ const actions = [
   "restores",
   "fixes",
 ];
+
+const randomWords = [
+  "cheese",
+  "royal",
+  "ocean",
+  "world",
+  "kingdom",
+  "monster",
+  "animal",
+  "farm",
+  "town",
+  "relative",
+];
+
+const getRandomProtagonist = () => {
+  let protagonist =
+    protagonists[Math.floor(Math.random() * protagonists.length)];
+  console.log(protagonist);
+};
+
+const getRandomAction = () => {
+  let action = actions[Math.floor(Math.random() * actions.length)];
+};
+
+const getRandomWord = () => {
+  let randomWord = randomWords[Math.floor(Math.random() * randomWords.length)];
+};
+
+const turnCube = () => {
+  const sideToShow = "show-" + document.getElementsByClassName("right").value;
+};
+
+const getStoryElements = () => {
+  document.getElementById("storyElementsButton");
+
+  getRandomProtagonist();
+  getRandomAction();
+  getRandomWord();
+  turnCube();
+};
