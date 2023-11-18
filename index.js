84146ac5e30295798344ee07c7e3a009402e8f39
holgermueller <9jholger@gmail.com>
2023-11-18 14:42:03 -0600

    added about modal

--- index.js
+++ index.js
@@ -136,3 +136,17 @@ const toggleButton = () => {
     getStoryElements();
   }
 };
+
+const modal = document.getElementById("myModal");
+
+const button = document.getElementById("modalButton");
+
+const closeButton = document.getElementById("span");
+
+const showModal = () => {
+  modal.style.display = "block";
+};
+
+const hideModal = () => {
+  modal.style.display = "none";
+};
