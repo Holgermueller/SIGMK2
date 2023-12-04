a3bd17e77079709639a69b34230f801a220c3093
holgermueller <9jholger@gmail.com>
2023-12-04 16:16:15 -0600

    updated footer

--- index.js
+++ index.js
@@ -150,3 +150,7 @@ const showModal = () => {
 const hideModal = () => {
   modal.style.display = "none";
 };
+
+const d = new Date();
+let year = d.getFullYear();
+document.getElementById("year").innerHTML = year;
